f71ff4c084e312e4eec98efa8263d9d829fe7d58
KonstantinFaleev <konstantin.faleev@gmail.com>
2017-11-27 00:14:52 +0200

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,8 @@ Rails.application.configure do
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
+      :user_name      => ENV['Konstantin Faleev'],
+      :password       => ENV['55g290808f'],
       :domain         => 'heroku.com',
       :enable_starttls_auto => true
   }

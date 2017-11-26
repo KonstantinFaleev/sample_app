90dac28b35c2c74fef3e8c6c8b9234f9c87358a9
KonstantinFaleev <konstantin.faleev@gmail.com>
2017-11-26 23:59:15 +0200

    Add account activation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171123080523) do
+ActiveRecord::Schema.define(version: 20171126182731) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 20171123080523) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
